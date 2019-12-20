90ff22f9d964de67bef19a142db87c31f8d3887c
dotvirus <33938500+dotvirus@users.noreply.github.com>
2019-12-20 14:48:17 +0100

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,29 +5,6 @@
       <v-toolbar-title v-text="title" />
     </v-app-bar>
 
-    <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">Application</v-list-item-title>
-          <v-list-item-subtitle>Subtitle</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-content>
       <nuxt />
     </v-content>
